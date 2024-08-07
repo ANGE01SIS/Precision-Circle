b15e7e0a5d06e210e239f61019a00557077ca04d
APoint <andressamaniegosandoval@gmail.com>
2024-08-08 00:11:08 +0200

    Empiezan las purebas en telefono

--- precision-circle/src/App.jsx
+++ precision-circle/src/App.jsx
@@ -1,35 +1,86 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [isPlayTheGame, setPlay] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="introduction-game">
+      <div className="titles-cont">
+        <h1 className="title-game">
+          {isPlayTheGame ? "Elije tu" : "Un juego de precisión"}
+        </h1>
+        <span
+          className={`txtGlitch ${
+            isPlayTheGame ? `isTextDificultad` : `isTextSimple`
+          }`}
+          dataglitch={isPlayTheGame ? "DIFICULTAD" : "SIMPLE"}
+        >
+          {isPlayTheGame ? "DIFICULTAD" : "SIMPLE"}
+        </span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <button
+        className={`play-game-button ${isPlayTheGame ? `desactive` : ``}`}
+        onClick={() => {
+          setPlay(!isPlayTheGame);
+        }}
+      >
+        PLAY
+      </button>
+      {isPlayTheGame ? <SelectDifficult /> : ""}
+    </main>
+  );
 }
 
-export default App
+export function SelectDifficult() {
+  return (
+    <div className="difficultCont">
+      <button className="difficultFacilButton buttonDificult">FACIL</button>
+      <button className="difficultNormalButton buttonDificult">NORMAL</button>
+      <button className="difficultDificilButton buttonDificult">DIFICIL</button>
+    </div>
+  );
+}
+
+export function CircleGame({ difficult }) {
+  const [position, setPosition] = useState({ y: -50, x: -50 });
+  const [size, setSize] = useState(200);
+  let maxY = window.innerHeight - size;
+  let maxX = window.innerWidth - size;
+
+  function getRandomPos() {
+    const YRandom = Math.floor(Math.random() * maxY - 10);
+    const XRandom = Math.floor(Math.random() * maxX - 10);
+    setPosition({ y: YRandom, x: XRandom });
+    if (size >= 50) {
+      setSize(size - 10);
+    } else if (size > 10 && size < 50) {
+      setSize(size - 2);
+    } else if (size === 10) {
+      setSize(size);
+    }
+  }
+
+  useEffect(() => {
+    maxX = window.innerHeight - size;
+    maxY = window.innerWidth - size;
+  }, [window.innerHeight, window.innerWidth]);
+
+  return (
+    <div
+      className="circle"
+      style={{
+        height: `${size}px`,
+        width: `${size}px`,
+        display: "inline-block",
+        backgroundColor: "white",
+        borderRadius: "50%",
+        transform: `translate(
+        ${position.x}px, 
+        ${position.y}px)`,
+      }}
+      onMouseEnter={getRandomPos}
+      onClick={getRandomPos}
+    ></div>
+  );
+}
+export default App;
